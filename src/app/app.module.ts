6bee4cc6d6c1549504c4adcad341b234214f3156
= <sivan.hari.sr@gmail.com>
2019-03-29 19:06:44 +0530

    learned @output and removed the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+import { EventsThumbnailComponent } from "./events/events-thumbnail.component";
 
 @NgModule({
-  declarations: [EventsAppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventsThumbnailComponent
+  ],
+  imports: [BrowserModule],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
